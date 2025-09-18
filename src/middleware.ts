574f947c9ba3e86ce6869b8cea91a25fa4142924
Serhii Zelik <40599524+ZelikSV@users.noreply.github.com>
2025-09-18 09:41:09 +0300

    removed logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,4 @@
 import {NextResponse, NextRequest} from 'next/server';
-import {log} from '@/lib/logger';
 
 export const middleware = (request: NextRequest) => {
     const {pathname} = request.nextUrl;
@@ -9,8 +8,6 @@ export const middleware = (request: NextRequest) => {
 
     const token = request.cookies.get('next-auth.session-token')?.value ||
         request.cookies.get('__Secure-next-auth.session-token')?.value;
-
-    log.debug('middleware', {token, isPublicPath, pathname});
 
     if (!token && !isPublicPath) {
         return NextResponse.redirect(new URL('/login', request.url));
